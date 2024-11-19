11d37d9c32e89e871db8a234c19bda75988be949
Margaux <margaux.espinasse@gmail.com>
2024-11-19 14:52:00 +0100

    MapView adjusted with ChangeView

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  map.setView(center, zoom);
+  return null;
+}
 
 export default function MapView({ coordinates }) {
   const position = [coordinates.lat, coordinates.lng];
+  const defaultPosition = [37.7749, -122.4194];
+  console.log('Map coordinates:', coordinates);
 
   return (
     <MapContainer
-      center={position}
+      center={coordinates.lat && coordinates.lng ? position : defaultPosition}
       zoom={13}
       style={{ height: '500px', width: '100%' }}
     >
+      <ChangeView center={position} zoom={13} />
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={position}>
         <Popup>
