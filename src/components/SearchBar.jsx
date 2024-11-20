6ab9a19e4d1d2f82211688ea2bf4aefbcb29ee08
Margaux <margaux.espinasse@gmail.com>
2024-11-20 10:50:41 +0100

    arrowIcon implemented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import arrowIcon from '../assets/icon-arrow.svg';
 
 export default function SearchBar({ onSearch }) {
   const [input, setInput] = useState('');
@@ -19,8 +20,10 @@ export default function SearchBar({ onSearch }) {
       />
       <button
         type="submit"
-        className="bg-black text-white px-4 py-2 rounded-r-md"
-      ></button>
+        className="bg-black text-white px-4 py-2 rounded-r-lg flex justify-center items-center"
+      >
+        <img src={arrowIcon} alt="submit button" className="w-3 h-3" />
+      </button>
     </form>
   );
 }
